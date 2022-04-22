46ba46e33aa6185187d0ba0fdf66f7f5b0684f56
Sharon Odima <odimasharon@gmail.com>
2022-04-22 17:02:19 -0400

    Sharon odima Completed Week2 Sprint Challenge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Character from './components/Character.js';
+import axios from "axios";
+import styled from 'styled-components';
+import './App.css';
+
+const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: center;    
+    width: 100%;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+      .then(response => {
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
+
+  let title = "REACT WARS  >";
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyledContainer className="App">
+      <h1 className="Header">{title}</h1>
+      {characters.map((character, idx) => <Character
+        key={idx}
+        info={character}
+        name={character.name}
+        birthYear={character.birth_year}
+        height={character.height}
+        mass = {character.mass}
+        hairColor = {character.hair_color}
+        skinColor = {character.skin_color}
+        gender = {character.gender}
+      />)}
+    </StyledContainer>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,64 @@
 // Write your Character component here
+import React, {useState} from 'react';
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-style: solid;
+    border-color: green;
+    border-width: 1px;
+    width: 60%;
+    margin-bottom: 20px;
+    padding: 10px;
+    align-self: center;
+    color: white
+  `
+const StyledHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    font-size: 20px;
+    font-family: 'Press Start 2P', cursive;
+`;
+
+const StyledBody = styled.div`
+  
+`
+
+const StyledName = styled.div`
+    
+`;
+
+const StyledButton = styled.button`
+    color: red;
+    background-color: #585A5A;
+    color: #361F51;
+    padding: 8px;
+    border-radius: 10px; 
+`;
+
+const Character = (props)=> {
+    const [open, setOpen] = useState(false);
+  return (
+    <StyledContainer>
+    <StyledHeader>
+        <StyledName>{props.name}</StyledName>
+        <StyledButton onClick={() => setOpen(!open)}>{props.birthYear}</StyledButton>
+    </StyledHeader>
+    {open && <StyledBody>
+        
+        <p>Height: {props.height}</p>
+        <p>Mass: {props.mass}</p>
+        <p>Hair Color: {props.hairColor} </p>
+        <p>Skin Color: {props.skinColor} </p>
+        <p>Gender: {props.gender} </p>
+
+    </StyledBody>
+    }
+    </StyledContainer>
+  );
+}
+
+export default Character; 
